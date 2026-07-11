a725f33ba0c7fe7b0b05a169802309370999662c
Jimena H <jghuarcaya@gmail.com>
2026-07-11 11:41:38 +0200

    fix: correct docker-compose indentation and create users table

---
+++ postgres-init/init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE userdb;
+
+\connect userdb
+
+CREATE TABLE IF NOT EXISTS users (
+                                     id SERIAL PRIMARY KEY,
+                                     name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+    );
